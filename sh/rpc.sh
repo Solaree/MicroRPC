0a6a861253f0c4f20f58526348106b87e8a991e2
Solar <115794865+Solaree@users.noreply.github.com>
2023-11-21 11:39:03 +0200

    Setup script for debian/ubuntu running fish shell

--- sh/rpc.sh
+++ sh/rpc.sh
@@ -1,4 +1,21 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install git
-git clone https://github.com/Solaree/MicroRPC.git ~/.config/micro/plug/
+sudo apt -y update && sudo apt -y upgrade
+sudo apt -y install python3 python3-pip
+pip install pypresence
+
+sudo chmod +x ./micro.sh
+sudo cp ./micro.sh /usr/local/bin/micro
+sudo mkdir -p $HOME/.config/micro/plug/MicroRPC
+sudo cp ../rpc/rpc.py $HOME/.config/micro/plug/MicroRPC/rpc.py
+
+SCRIPT_PATH="/usr/local/bin/micro"
+CONTENT="alias micro $SCRIPT_PATH"
+CONFIG="$HOME/.config/fish/config.fish"
+
+if [ -e "$CONFIG" ]; then
+    echo "$CONTENT" >> "$CONFIG"
+else
+    echo "$CONTENT" > "$CONFIG"
+fi
+
+clear
+echo "\033[0;102mMicroRPC: Script finished. Please restart your terminal or run 'source ~/.config/fish/config.fish' to apply the changes."
